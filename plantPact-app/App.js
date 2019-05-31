698f84deef38031a0f19a4f28501bc261a690f6b
David Spencer <david-spencer@davids-imac.home>
2019-05-31 13:03:47 +0100

    basic implementation of screens with navigation

--- plantPact-app/App.js
+++ plantPact-app/App.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  createAppContainer,
+  createStackNavigator,
+  createSwitchNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+import Ionicons from "react-native-vector-icons/Ionicons"
+
+import WelcomeScreen from "./screens/WelcomeScreen";
+import BasketScreen from "./screens/BasketScreen"
+import CheckoutScreen from "./screens/CheckoutScreen"
+import ProductDetailsScreen from "./screens/ProductDetailsScreen"
+
+const MainStack = createStackNavigator(
+  {
+    Basket: BasketScreen,
+    ProductDetails: ProductDetailsScreen
+  },
+  {
+    initialRouteName: "Basket"
+  }
+);
+
+MainStack.navigationOptions = {
+  tabBarIcon: ({ focused, tintColor }) => (
+    <Ionicons
+      name={"ios-basket"}
+      size={25}
+      color={tintColor}
+    />
+  )
+};
+
+const MainTabs = createBottomTabNavigator(
+  {
+    Products: MainStack,
+    Checkout: CheckoutScreen
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: "#2b4116"
+    }
+  }
+);
+
+const AppNavigator = createSwitchNavigator({
+  Welcome: WelcomeScreen,
+  Main: MainTabs
+});
+
+const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
+  state = {
+
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppContainer
+        screenProps={{}}
+      />
     );
   }
 }
